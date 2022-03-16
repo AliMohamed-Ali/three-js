1b0ee827bc89ab0bb4dcea9611e91d29111b5195
AliMohamed-Ali <elsayedali3632@gmail.com>
2022-03-16 23:05:04 +0200

    upload file aand texture

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var theModel;
-let MODEL_PATH ='table.glb'
+let MODEL_PATH = './table.glb'
 const BACKGROUND_COLOR = 0xf1f1f1;
 const header = document.querySelector('#header')
 var loaded = false;
@@ -8,89 +8,89 @@ const DRAG_NOTICE = document.getElementById('js-drag-notice');
 const scene = new THREE.Scene();
 
 // Set background
-scene.background = new THREE.Color(BACKGROUND_COLOR );
+scene.background = new THREE.Color(BACKGROUND_COLOR);
 scene.fog = new THREE.Fog(BACKGROUND_COLOR, 20, 100);
 
 const canvas = document.querySelector('#c');
 // Init the renderer
-const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 renderer.shadowMap.enabled = true;
 renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 var cameraFar = 5;
 // Add a camera
-var camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = cameraFar;
 camera.position.x = 0;
 // Initial material
-const INITIAL_MTL = new THREE.MeshPhongMaterial( { color: 0xf1f1f1, shininess: 10 } );
+const INITIAL_MTL = new THREE.MeshPhongMaterial({ color: 0xf1f1f1, shininess: 10 });
 const INITIAL_MAP = [
-  {childID: "back", mtl: INITIAL_MTL},
-  {childID: "base", mtl: INITIAL_MTL},
-  {childID: "cushions", mtl: INITIAL_MTL},
-  {childID: "leg", mtl: INITIAL_MTL},
-  {childID: "support", mtl: INITIAL_MTL},
-  {childID: "table", mtl: INITIAL_MTL},
+  { childID: "back", mtl: INITIAL_MTL },
+  { childID: "base", mtl: INITIAL_MTL },
+  { childID: "cushions", mtl: INITIAL_MTL },
+  { childID: "leg", mtl: INITIAL_MTL },
+  { childID: "support", mtl: INITIAL_MTL },
+  { childID: "table", mtl: INITIAL_MTL },
 ];
 // Init the object loader
-var loader = new THREE.GLTFLoader();
-loader.load(MODEL_PATH, function(gltf) {
-  theModel = gltf.scene;
-  console.log(theModel)
-  theModel.traverse((o) => {
-    if (o.isMesh) {
-      o.castShadow = true;
-      o.receiveShadow = true;
-    }
-  });
+  var loader = new THREE.GLTFLoader();
+  loader.load(MODEL_PATH, function (gltf) {
+    theModel = gltf.scene;
+    // console.log(theModel)
+    theModel.traverse((o) => {
+      if (o.isMesh) {
+        o.castShadow = true;
+        o.receiveShadow = true;
+      }
+    });
 
-// Set the models initial scale   
-  // theModel.scale.set(.5,.5,.5);
-  theModel.scale.set(1,1,1);
-  theModel.rotation.y = Math.PI;
+    // Set the models initial scale   
+    // theModel.scale.set(.5,.5,.5);
+    theModel.scale.set(1, 1, 1);
+    theModel.rotation.y = Math.PI;
 
-  // Offset the y position a bit
-  theModel.position.y = -1;
-  // Set initial textures
-  for (let object of INITIAL_MAP) {
-    initColor(theModel, object.childID, object.mtl);// Set initial textures
+    // Offset the y position a bit
+    theModel.position.y = -1;
+    // Set initial textures
     for (let object of INITIAL_MAP) {
-      initColor(theModel, object.childID, object.mtl);
+      initColor(theModel, object.childID, object.mtl);// Set initial textures
+      for (let object of INITIAL_MAP) {
+        initColor(theModel, object.childID, object.mtl);
+      }
     }
-  }
-  renderDom(theModel)
+    renderDom(theModel)
 
 
-  // Add the model to the scene
-  scene.add(theModel);
-  
+    // Add the model to the scene
+    scene.add(theModel);
 
-}, undefined, function(error) {
-  console.error(error)
-});
+
+  }, undefined, function (error) {
+    console.error(error)
+  });
 // Function - Add the textures to the models
 function initColor(parent, type, mtl) {
   parent.traverse((o) => {
-   if (o.isMesh) {
-     if (o.name.toLowerCase().includes(type)) {
-          o.material = mtl;
-          o.nameID = type; // Set a new property to identify this object
-       }
-   }
- });
+    if (o.isMesh) {
+      if (o.name.toLowerCase().includes(type)) {
+        o.material = mtl;
+        o.nameID = type; // Set a new property to identify this object
+      }
+    }
+  });
 }
 // Add lights
-var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.61 );
-    hemiLight.position.set( 0, 50, 0 );
+var hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.61);
+hemiLight.position.set(0, 50, 0);
 // Add hemisphere light to scene   
-scene.add( hemiLight );
+scene.add(hemiLight);
 
-var dirLight = new THREE.DirectionalLight( 0xffffff, 0.54 );
-    dirLight.position.set( -8, 12, 8 );
-    dirLight.castShadow = true;
-    dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+var dirLight = new THREE.DirectionalLight(0xffffff, 0.54);
+dirLight.position.set(-8, 12, 8);
+dirLight.castShadow = true;
+dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
 // Add directional Light to scene    
-    scene.add( dirLight );
+scene.add(dirLight);
 // Floor
 var floorGeometry = new THREE.PlaneGeometry(5000, 5000, 1, 1);
 var floorMaterial = new THREE.MeshPhongMaterial({
@@ -104,7 +104,7 @@ floor.receiveShadow = true;
 floor.position.y = -1;
 scene.add(floor);
 // Add controls
-var controls = new THREE.OrbitControls( camera, renderer.domElement );
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.maxPolarAngle = Math.PI / 2;
 controls.minPolarAngle = Math.PI / 3;
 controls.enableDamping = true;
@@ -112,69 +112,118 @@ controls.enablePan = false;
 controls.dampingFactor = 0.1;
 controls.autoRotate = false; // Toggle this if you'd like the chair to automatically rotate
 controls.autoRotateSpeed = 0.2; // 30
-    function animate() {
-        controls.update();
-        renderer.render(scene, camera);
-        requestAnimationFrame(animate);
-        
-        if (resizeRendererToDisplaySize(renderer)) {
-          const canvas = renderer.domElement;
-          camera.aspect = canvas.clientWidth / canvas.clientHeight;
-          camera.updateProjectionMatrix();
-        }
-        if (theModel != null && loaded == false) {
-            initialRotation();
-            DRAG_NOTICE.classList.add('start');
-          }
-      }
-  
-  animate();
-  function resizeRendererToDisplaySize(renderer) {
+function animate() {
+  controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+
+  if (resizeRendererToDisplaySize(renderer)) {
     const canvas = renderer.domElement;
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    var canvasPixelWidth = canvas.width / window.devicePixelRatio;
-    var canvasPixelHeight = canvas.height / window.devicePixelRatio;
-  
-    const needResize = canvasPixelWidth !== width || canvasPixelHeight !== height;
-    if (needResize) {
-      
-      renderer.setSize(width, height, false);
-    }
-    return needResize;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
   }
-  function renderDom(obj){
-    const pickColor = document.querySelector('#pickColor')
-
-    pickColor.addEventListener('input',function(e){
-        const mtl = new THREE.MeshPhongMaterial( { color: e.target.value, shininess: 10 } );
-        obj.children.forEach(el=>{
-            el.material = mtl
-        })
-    })
-
-    obj.children.forEach(el=>{
-        const label = document.createElement('label')
-        label.textContent = el.name
-        const checkbox = document.createElement('input')
-        checkbox.setAttribute('type','checkbox')
-        checkbox.checked = el.visible
-        checkbox.addEventListener('change',function(){
-            el.visible = !el.visible
-        })
-        label.appendChild(checkbox)
-        header.appendChild(label)
-    })
-      
+  if (theModel != null && loaded == false) {
+    initialRotation();
+    DRAG_NOTICE.classList.add('start');
   }
-  // Function - Opening rotate
+}
+
+animate();
+function resizeRendererToDisplaySize(renderer) {
+  const canvas = renderer.domElement;
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var canvasPixelWidth = canvas.width / window.devicePixelRatio;
+  var canvasPixelHeight = canvas.height / window.devicePixelRatio;
+
+  const needResize = canvasPixelWidth !== width || canvasPixelHeight !== height;
+  if (needResize) {
+
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+function renderDom(obj) {
+  const pickColor = document.querySelector('#pickColor')
+
+  pickColor.addEventListener('input', function (e) {
+    const mtl = new THREE.MeshPhongMaterial({ color: e.target.value, shininess: 10 });
+    obj.children.forEach(el => {
+      el.material = mtl
+    })
+  })
+
+  obj.children.forEach(el => {
+    const label = document.createElement('label')
+    label.textContent = el.name
+    const checkbox = document.createElement('input')
+    checkbox.setAttribute('type', 'checkbox')
+    checkbox.checked = el.visible
+    checkbox.addEventListener('change', function () {
+      el.visible = !el.visible
+    })
+    label.appendChild(checkbox)
+    header.appendChild(label)
+  })
+
+}
+// Function - Opening rotate
 let initRotate = 0;
 
 function initialRotation() {
   initRotate++;
-if (initRotate <= 120) {
+  if (initRotate <= 120) {
     theModel.rotation.y += Math.PI / 60;
   } else {
     loaded = true;
   }
 }
+document.querySelector('#texture').addEventListener('change', function (e) {
+  let userImage = e.target.files[0];
+  let userImageURL = URL.createObjectURL(userImage);
+
+  let loader = new THREE.TextureLoader();
+  loader.setCrossOrigin("");
+  let texture = loader.load(userImageURL);
+
+  // update texture
+  // shader.uniforms.texture1.value = texture;
+  theModel.children.forEach(el => {
+    el.material = new THREE.MeshPhongMaterial({ map: texture });
+  })
+})
+
+
+document.querySelector('#model').addEventListener('change', function (e) {
+  //   let file = e.target.files[0]
+  //   let reader = new FileReader();
+  //   // This is code that runs after reader.readAsText() finishes
+  //   reader.onload = function ( gltfText ) {
+
+  //     var loader = new THREE.GLTFLoader();
+
+  //     loader.parse( gltfText, '', function( gltf ){
+
+  //       scene.add( gltf.scene );
+  //       renderer.render( scene, camera );
+
+  //     }, function( errormsg ){
+  //         console.error( errormsg );
+  //     });    
+
+  // };
+
+  // reader.readAsText( file );
+  let file = e.target.files[0];
+  let reader = new FileReader();
+  reader.readAsArrayBuffer(file);
+  reader.onload = gltfText => {
+    let loader = new THREE.GLTFLoader();
+    loader.parse(gltfText.target.result, '', (gltfData) => {
+      scene.add(gltfData.scene)
+      renderer.render( scene, camera );
+
+    },
+      errMassage => { console.error(errMassage) })
+  }
+})
